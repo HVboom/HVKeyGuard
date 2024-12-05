ff88cdb0f02b0ec310ea25d10e82ba1b294fd9f8
Mario <Mario@HVboom.ch>
2024-12-05 16:51:00 +0100

    Demo App: Log to file

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -3,4 +3,13 @@ require_relative 'production'
 Rails.application.configure do
   # config settings specific to
   # the demo environment.
+
+  # Log to STDOUT by default
+  # config.logger = ActiveSupport::Logger.new(STDOUT)
+  # Log to file
+  config.logger = ActiveSupport::Logger.new("log/demo.log")
+    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "debug")
 end
